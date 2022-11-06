6e113a2e8dbf6ac997a152cb47747dbd95fe8c90
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-05 22:43:42 -0500

    resume @ L125

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,7 +1,33 @@
-import React from "react"
+import React, { useState } from "react"
 
 const AuthContext = React.createContext({
   isLogged: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 })
+
+export const AuthContentProvoder = props => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false)
+  }
+
+  const loginHandler = () => {
+    setIsLoggedIn(true)
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: loginHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
 
 export default AuthContext
