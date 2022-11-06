778beb760c286dea341224480297be8e097e635e
Lawrence O. Caulker <locaulker@gmail.com>
2022-11-05 20:52:19 -0400

    introduced context API

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,29 +1,36 @@
-import React from 'react';
+import React from "react"
 
-import classes from './Navigation.module.css';
+import AudioContext from "../../store/auth-context"
+import classes from "./Navigation.module.css"
 
-const Navigation = (props) => {
+const Navigation = props => {
   return (
-    <nav className={classes.nav}>
-      <ul>
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Users</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <a href="/">Admin</a>
-          </li>
-        )}
-        {props.isLoggedIn && (
-          <li>
-            <button onClick={props.onLogout}>Logout</button>
-          </li>
-        )}
-      </ul>
-    </nav>
-  );
-};
+    <AudioContext.Consumer>
+      {ctx => {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {ctx.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        )
+      }}
+    </AudioContext.Consumer>
+  )
+}
 
-export default Navigation;
+export default Navigation
